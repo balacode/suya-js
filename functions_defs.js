f8eb382a0ffe343497d08e7ff7e7840b02d26b8a
balarabe@protonmail.com <balarabe@protonmail.com>
2018-02-25 15:13:15 +0100

    Add functions_defs.js and jscheck.bat (for use with Google Closure Compiler)

---
+++ functions_defs.js
@@ -0,0 +1,67 @@
+// -----------------------------------------------------------------------------
+// (c) balarabe@protonmail.com                                      License: MIT
+// :v: 2018-02-25 15:10:49 6ED9C5                            [functions_defs.js]
+// -----------------------------------------------------------------------------
+
+/*
+    This file just specifies some definitions to help
+    Google Closure Compiler to type-check functions.js
+
+    They are not defined directly in functions.js
+    because they are not needed for functionality.
+*/
+
+var zr  = {};
+
+var sessionStorage  = {
+    setItem:  function(a, b) {}
+};
+
+/** ITextRange: __
+ *  @typedef
+ *  {{
+ *      duplicate: function():Object,
+ *      move:      function(string, number),
+ *      moveStart: function(string, number),
+ *      select:    function(),
+ *      text:      string,
+ *  }}
+ */
+zr.ITextRange;
+
+/** IWindow: __
+ *  @typedef
+ *  {{
+ *      event: Event,
+ *      frames: Array<Object>
+ *  }}
+ */
+zr.IWindow;
+
+/** ListRows: __
+ *  @typedef !Array<!Array<!string>>
+ */
+zr.ListRows;
+
+/** Property: Single property name and value.
+ *  Returned by properties() function.
+ *  @typedef
+ *  {{
+ *      name:  string,
+ *      value: *,
+ *  }}
+ */
+zr.Property;
+
+/** Response: response to a HTTP request.
+ *  @typedef
+ *  {{
+ *      err:         number,
+ *      status:      number,
+ *      status_text: string,
+ *      text:        string
+ *  }}
+ */
+zr.Response;
+
+//end
---
+++ jscheck.bat
@@ -0,0 +1,26 @@
+@rem  [jscheck.bat]
+@rem
+@rem  Uses the Google Colsure Compiler
+@rem  to type-check functions.js
+@rem
+@rem  To be able to do this, you have to download
+@rem  and set up the path of closure-compiler.jar
+@rem
+@rem  Though this is a Windows batch file, the 'java'
+@rem  command syntax on other platforms is the same
+
+cls
+
+java -jar closure-compiler.jar ^
+        --compilation_level SIMPLE ^
+        --checks-only ^
+        --jscomp_warning=reportUnknownTypes -W VERBOSE ^
+        ^
+        functions_defs.js ^
+        functions.js ^
+        ^
+        2>> jscheck.txt
+
+type jscheck.txt
+
+@ rem end
